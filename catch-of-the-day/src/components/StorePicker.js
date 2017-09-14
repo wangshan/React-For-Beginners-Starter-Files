73b7b216016f4929b29ce8dd0a6e313c9eebbdc9
Shan Wang <uniwangshan@gmail.com>
2017-09-14 17:33:18 +0100

    Capture event and use context

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    constructor() {
+        super();
+        // need to do this for every method
+        this.goToStore = this.goToStore.bind(this);
+    }
+
+    goToStore(event) {
+        event.preventDefault();
+        // grab text from box
+        // change url
+        console.log("You changed the URL");
+        console.log(this.storeInput.value);
+        const storeId = this.storeInput.value;
+        this.context.router.transitionTo(`/store/${storeId}`);
+    }
+
     render() {
         return (
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
+                {/* onSubmit = {(e) => {this.goToStore(e)}} */}
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input type="text" required placeholder="Store Name"
+                    defaultValue={getFunName()}
+                    ref={(input) => {this.storeInput = input}}
+                />
                 <button type="submit">Visit Store</button>
             </form>
         )
     }
+}
+
+// react made this hard to use because context is usually discoveraged
+StorePicker.contextTypes = {
+    router: React.PropTypes.object
 }
 
 export default StorePicker;
